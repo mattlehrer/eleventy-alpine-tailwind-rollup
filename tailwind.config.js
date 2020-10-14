6c2acd93336855098a42064ed8cac2699f3d55a0
Stefan Frede <stefan@frede.dev>
2020-10-14 11:26:49 +0200

    maintenance (#28)
    * chore: update node modules
    
    * chore: accept all future changes in tailwind
    
    * chore: udpate npm version
    
    * refactor: clean-up auto-format mess
    
    * refactor: rename scaffold to base
    
    refactor: update page structure (#29)
    
    * refactor: update page structure
    
    * fix: sync package-lock.json
    
    refactor: beautify 404 page (#30)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  future: {
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true,
-  },
+  future: 'all',
   purge: ['./src/**/*.html', './src/**/*.md'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        '7xl': '5rem',
+      },
+      gridTemplateRows: {
+        layout: 'auto 1fr auto',
+      },
       inset: {
         '-20': '-5rem',
       },
       margin: {
-        '-2px': '-2px',
+        '-1px': '-1px',
       },
     },
   },
